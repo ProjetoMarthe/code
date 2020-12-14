9406ce504d3c3d7449a73f61eeee78004f4c1aba
Juliana <jubrito@usp.br>
2020-12-14 16:32:35 -0300

    ajustes finais

--- mobile/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const IPAddress = '192.168.15.83';
-
-const api = axios.create({
-    baseURL: `http://${IPAddress}:3333` // endereço pegado do expo que é o nosso ip 
-});
-
-export default api
